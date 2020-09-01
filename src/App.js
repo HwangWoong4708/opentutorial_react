d28fa189a5b28d3dd300ff75ee84cc82467f9bf5
HwangWoong <hwang_woong@naver.com>
2020-09-01 16:04:32 +0900

    make Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import React from "react";
-
 import "./App.css";
+
+class Subject extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends React.Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <div className="App">
-          <h1>Hello, React!</h1>
-        </div>
+      <div className="App">
+        <Subject />
+        <Nav />
+        <Article />
       </div>
     );
   }
