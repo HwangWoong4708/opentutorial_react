d795306e43feb291e15e8de4e1eb573db8d63fe9
HwangWoong <hwang_woong@naver.com>
2020-09-01 15:28:13 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <h1>Hello, React!</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
